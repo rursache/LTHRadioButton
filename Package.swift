2c7e99863fc90419db47f7c05a7b669b1d613aaa
Radu Ursache <3800336+rursache@users.noreply.github.com>
2021-02-08 12:35:30 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+  name: "LTHRadioButton",
+  products: [
+    .library(name: "LTHRadioButton", targets: ["LTHRadioButton"]),
+  ],
+  targets: [
+    .target(
+      name: "LTHRadioButton",
+      dependencies: [],
+      path: "source"),
+  ]
+)
